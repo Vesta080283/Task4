8f984c05122efcfac67b33807565a1d1d20eeac9
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2019-05-16 12:58:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let table = document.querySelector('.chess-board');
 let selTd;
+let cellSteps = [];
 
 table.onclick = (event) => {
     let target = event.target;
@@ -10,32 +11,36 @@ table.onclick = (event) => {
     if (!table.contains(td)) return;// нашли элемент, который нас интересует!
 
     highlightSel(td);
-    highlightStep(getSteps(td));
+    highlightStep(td);
 }
 
-const highlightSel = (cell) => {
-    console.log(cell.cellIndex);
+const highlightSel = (_selTd) => {
+    console.log(_selTd.cellIndex);
     if (selTd) {
-        selTd.classList.remove('highlight');
+        //selTd.classList.remove('highlight');
+        selTd.style.backgroundColor = '';
     }
-    selTd = cell;
-    selTd.classList.add('highlight');
-    //selTd.style.backgroundColor = 'blue';
+    selTd = _selTd;
+    //selTd.classList.add('highlight');
+    selTd.style.backgroundColor = 'blue';
 }
 
-const highlightStep = (indexes) => {
-    console.log(...indexes);
-    let cellStep;
-    if (!indexes == []) {
-        indexes.forEach((index) => {
-            cellStep = table.rows[index[0]].cells[index[1]];
-            cellStep.classList.remove('highlight-step');
+const highlightStep = (_selTd) => {
+    if (!cellSteps == []) {
+        cellSteps.forEach((cell) => {
+            cell = table.rows[cell[0]].cells[cell[1]];
+            //cell.classList.remove('highlight-step');
+            cell.style.backgroundColor = '';
         });
     }
-    indexes.forEach((index) => {
-        cellStep = table.rows[index[0]].cells[index[1]];
-        cellStep.classList.add('highlight-step');
-        //cellStep.style.backgroundColor = 'aqua';
+
+    cellSteps = getSteps(_selTd);
+    console.log(...cellSteps);
+
+    cellSteps.forEach((cell) => {
+        cell = table.rows[cell[0]].cells[cell[1]];
+        //cellStep.classList.add('highlight-step');
+        cell.style.backgroundColor = 'green';
     });
 }
 
@@ -49,10 +54,10 @@ const getSteps = (cell) => {
     let steps = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
 
     steps.forEach((step) => {
-        if (((x = rIndex + step[0]) >= 2 &&
-            (x = rIndex + step[0]) <= 10) &&
-            ((y = cIndex + step[1]) >= 2 &&
-                (y = cIndex + step[1]) <= 10)) {
+        if (((x = rIndex + step[0]) >= 1 &&
+            (x = rIndex + step[0]) <= 8) &&
+            ((y = cIndex + step[1]) >= 1 &&
+                (y = cIndex + step[1]) <= 8)) {
             result.push([x, y]);
         }
     });
